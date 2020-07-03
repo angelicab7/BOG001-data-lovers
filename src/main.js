6a02273951ca56f089488681249ee43a1d480cbc
unknown <dromerovarela776@gmail.com>
2020-07-03 08:24:53 -0500

    Master

---
+++ src/main.js
@@ -0,0 +1,13 @@
+
+const buttonMenu = document.querySelector('#main-menu-button');
+
+buttonMenu.addEventListener('click', toggleMenu);
+
+/**
+ * Toggles main menu
+ * @param {Event} event 
+ */
+function toggleMenu(event) {
+    const mainMenu = document.querySelector('.main-menu');
+    mainMenu.classList.toggle('active');
+}
