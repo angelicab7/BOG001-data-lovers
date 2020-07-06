73854bbf9961ef442520c63dc67c94e803b65a0b
unknown <dromerovarela776@gmail.com>
2020-07-06 11:34:05 -0500

    slider y footer

--- src/main.js
+++ src/main.js
@@ -11,3 +11,25 @@ function toggleMenu(event) {
     const mainMenu = document.querySelector('.main-menu');
     mainMenu.classList.toggle('active');
 }
+
+//funcion slider//}
+
+const slides=document.querySelector(".slider").children;
+const indicator=document.querySelector(".slider-indicator").children;
+
+ for(let i=0; i<indicator.length; i++){
+     indicator[i].addEventListener("click",function(){
+      
+        for(let j=0; j<indicator.length; j++){
+          indicator[j].classList.remove("active");
+        }
+         this.classList.add("active");
+         const id=this.getAttribute("data-id");
+        for(let j=0; j<slides.length; j++){
+            slides[j].classList.remove("active");
+        }
+
+         slides[id].classList.add("active");
+
+     })
+ }
