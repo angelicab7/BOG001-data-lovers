db0da0fa24f32bedf85bd2df50431a63b23ac156
Angelica Bonilla Riaño <angelicabonilla723@gmail.com>
2020-07-09 09:00:45 -0500

    mostrar datos en el html

--- src/data.js
+++ src/data.js
@@ -1,3 +1,30 @@
+async function getCharacters() {
+    let charactersContainer = document.querySelector('#characters-container')
+    const response = await fetch('https://rickandmortyapi.com/api/character');
+    const data = await response.json();
+    console.log(data);
+    data.results.forEach(character =>
+        charactersContainer.innerHTML += cardCharacter(character.image, character.name, character.status)
+    );
+};
+
+getCharacters();
 
 
-
+function cardCharacter(urlImage, title, description) {
+    return (
+        `
+        <div class="column">
+                <div class="card">
+                    <img src="${urlImage}" alt="image" class="card-image" />
+                    <div class="card-content">
+                        <h3 class="card-title">${title}</h3>
+                        <p class="card-description">
+                            ${description}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        `
+    )
+};
--- src/main.js
+++ src/main.js
@@ -1,32 +1,3 @@
-
-//halar data//
-
-let element = document.getElementById('personajitos')
-
-for (var i = 1; i < 10; i++) {
-    fetch('https://rickandmortyapi.com/api/character/' + i + '/').then(response => {
-        response.json().then(data => {
-            debugger
-            element.innerHTML = `
-            <div>
-                <img src='${data.image}'/>
-                <p>${data.name}</p>
-                <p>${data.status}</p>
-                <p>${data.species}</p>
-                <p>${data.type}</p>
-                <p>${data.gender}</p>
-                <p>${data.origin.name}</p>
-                
-            </div>
-            `;
-            console.log(data)
-        });
-    })
-        .catch(err => console.log(err));
-}
-
-
-
 /* menu*/
 
 const buttonMenu = document.querySelector('#main-menu-button');
