9efd33fcb984056da4e12a9d3157be5e0da447aa
Angelica Bonilla Riaño <angelicabonilla723@gmail.com>
2020-07-29 09:34:16 -0500

    Charts and last changes

--- src/slider.js
+++ src/slider.js
@@ -27,3 +27,33 @@ function muestraSlides(n) {
     slides[indice - 1].style.display = 'block';
     barras[indice - 1].className += ' active';
 }
+
+/*CHARTS*/
+
+const ctx = document.getElementById('myChart').getContext('2d');
+Chart.defaults.global.defaultFontColor = '#000000';
+
+const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Humans", "Alien", "Humanoid", "Robot", "Animal","Croenberg", "Poopybuthole", "Disease", "Unknown", "Vampire"],
+        datasets: [{
+          label: 'Kinds of species',
+          data: [244, 132, 53, 17, 11, 8,8, 6, 5, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.6)',
+            'rgba(54, 162, 235, 0.6)',
+            'rgba(255, 206, 86, 0.6)',
+            'rgba(75, 192, 192, 0.6)',
+            'rgba(153, 102, 255, 0.6)',
+            'rgba(255, 159, 64, 0.6)',
+            'rgba(255, 99, 132, 0.6)',
+            'rgba(54, 162, 235, 0.6)',
+            'rgba(255, 206, 86, 0.6)',
+            'rgba(75, 192, 192, 0.6)',
+            'rgba(153, 102, 255, 0.6)'
+          ]
+          
+        }]
+      }
+    }); 
